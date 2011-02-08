13d48b8d5707902b1a07d892fb0e279ff9453aba
Christopher League <league@contrapunctus.net>
2011-02-08 17:09:41 -0500

    improvements to matrix testing

--- src/test/scala/SquareMatrixTest.scala
+++ src/test/scala/SquareMatrixTest.scala
@@ -6,23 +6,29 @@ object QuadTreeCreateSpec extends Properties("quad tree create") {
   type M[A] = QuadTreeSquareMatrix[A]
   val factory = QuadTreeSquareMatrix
 
-  val powerOfTwo = Gen.choose(0,10).map(1 << _)
+  val powerOfTwo = Gen.choose(0,6).map(1 << _)
   val fillMatrix = for { n <- powerOfTwo ; c <- Gen.alphaChar }
                    yield factory.fill(n)(c)
-  val pairMatrix = powerOfTwo.map(factory.tabulate(_){(i,j)=>(i,j)})
+  def pairs(n: Int) = factory.tabulate(n){(i,j)=>(i,j)}
+  val pairMatrix = powerOfTwo.map(pairs)
   def coord[A](m: M[A]) = for {i <- Gen.choose(0, m.height-1);
                                j <- Gen.choose(0, m.width-1)}
                           yield (i,j)
 
   property("bounds") = Prop.forAll(fillMatrix) {
     m:M[Char] => Prop.forAll(coord(m)) {
-      ij => m(ij) == m(0,0)
+      ij =>
+        ij._1 >= 0 && ij._2 >= 0 ==> // mysteriously, sometimes gets -1
+          (m(ij) == m(0,0))
     }
   }
 
   property("coords") = Prop.forAll(pairMatrix) {
     m => Prop.forAll(coord(m)) {
-      ij => m(ij) == ij
+      ij =>
+        ij._1 >= 0 && ij._2 >= 0 ==> // mysteriously, sometimes gets -1
+          (m(ij) == ij)
     }
   }
+
 }
