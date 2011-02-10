f4255fcda02e208f3efc9d1da417467d5aaea13a
Christopher League <league@contrapunctus.net>
2011-02-09 20:56:03 -0500

    Reorganized sized vector operations and tests.

--- src/main/scala/okasaki-fast.scala
+++ src/main/scala/okasaki-fast.scala
@@ -1,27 +1,35 @@
 
+/** Operations on general 2D matrices. */
 trait Matrix[A] extends Function2[Int,Int,A] {
+  type M[X] <: Matrix[X]
   def width: Int
   def height: Int
   def size: (Int,Int) = (width, height)
   def apply(coord: (Int,Int)): A = apply(coord._1, coord._2)
-  def updated(i: Int, j: Int, elem: A): Matrix[A]
-  def map[B](f: A => B): Matrix[B]
+  def updated(i: Int, j: Int, elem: A): M[A]
+  def map[B](f: A => B): M[B]
 }
 
-trait MatrixFactory[M[A] <: Matrix[A]] {
+/** Constructors for general 2D matrices. */
+trait MatrixFactory {
+  type M[X] <: Matrix[X]
   def fill[A](n1: Int, n2: Int)(elem: => A): M[A]
   def tabulate[A](n1: Int, n2: Int)(f:(Int,Int) => A): M[A]
 }
 
+/** A square matrix has a single dimension (i.e., width == height). */
 trait SquareMatrix[A] extends Matrix[A] {
+  type M[X] <: SquareMatrix[X]
   def dimension: Int
   override def width: Int = dimension
   override def height: Int = dimension
 }
 
-trait SquareMatrixFactory[M[A] <: SquareMatrix[A]]
-extends MatrixFactory[M] {
-  def fill[A](n: Int)(elem: => A): M[A]
+/** Constructors for square matrices. */
+trait SquareMatrixFactory extends MatrixFactory {
+  type M[X] <: SquareMatrix[X]
+  def fill[A](n: Int)(elem: => A): M[A] =
+    tabulate(n){(i,j) => elem}
   def fill[A](n1: Int, n2: Int)(elem: => A): M[A] = {
     require(n1 == n2)
     fill(n1)(elem)
@@ -33,80 +41,66 @@ extends MatrixFactory[M] {
   }
 }
 
+/** Standardizes the errors raised by matrix implementations. */
 object MatrixErrors {
   def outOfBounds = error("out of bounds")
   def notImplemented = error("not implemented")
 }
 
-trait Quad[A] {
-  type Tup = (A,A,A,A)
-  def exponent: Int
-  def dimension = 1 << exponent
-}
+object QuadTreeSquareMatrix extends SquareMatrixFactory {
 
-abstract class QuadTreeSquareMatrix[A]
-extends SquareMatrix[A] with Quad[A] {
-  def updated(i: Int, j: Int, f: A => A): QuadTreeSquareMatrix[A]
-  def updated(i: Int, j: Int, elem: A): QuadTreeSquareMatrix[A] =
-    updated(i, j, (_:A) => elem)
-  def map[B](f: A => B): QuadTreeSquareMatrix[B]
-}
+  type M[A] = QT_Matrix[A]
 
-case class QT_Zero[A](data: A) extends QuadTreeSquareMatrix[A] {
-  val exponent: Int = 0
-  override def toString: String = "Zero " + data.toString
-  def apply(i: Int, j: Int): A =
-    if(i==0 && j==0) data
-    else MatrixErrors.outOfBounds
-  def updated(i: Int, j: Int, f: A => A): QuadTreeSquareMatrix[A] =
-    QT_Zero(f(data))
-  def map[B](f: A => B): QuadTreeSquareMatrix[B] =
-    QT_Zero(f(data))
-}
+  sealed abstract class QT_Matrix[A] extends SquareMatrix[A] {
+    type M[X] = QT_Matrix[X]
+    def exponent: Int
+    def dimension = 1 << exponent
+    def updated(i: Int, j: Int, f: A => A): M[A]
+    def updated(i: Int, j: Int, elem: A): M[A] =
+      updated(i, j, (_:A) => elem)
+  }
 
-case class QT_Succ[A](
-  subm: QuadTreeSquareMatrix[Quad[A]#Tup]
-) extends QuadTreeSquareMatrix[A] {
-  val exponent = 1 + subm.exponent
-  override def toString: String= "Succ " + subm.toString
-  def apply(i: Int, j: Int): A = {
-    val q = subm(i/2, j/2)
-    (i%2, j%2) match {
-      case (0,0) => q._1
-      case (0,1) => q._2
-      case (1,0) => q._3
-      case (1,1) => q._4
+  private case class Zero[A](data: A) extends QT_Matrix[A] {
+    val exponent: Int = 0
+    override def toString: String = "Zero " + data.toString
+    def apply(i: Int, j: Int): A =
+      if(i==0 && j==0) data
+      else MatrixErrors.outOfBounds
+    def updated(i: Int, j: Int, f: A => A): M[A] = Zero(f(data))
+    def map[B](f: A => B): M[B] = Zero(f(data))
+  }
+
+  type Quad[A] = (A,A,A,A)
+
+  private case class Succ[A](next: QT_Matrix[Quad[A]]) extends QT_Matrix[A] {
+    val exponent = 1 + next.exponent
+    override def toString: String= "Succ " + next.toString
+    def apply(i: Int, j: Int): A = (next(i/2, j/2), i%2, j%2) match {
+      case ((q,_,_,_),0,0) => q
+      case ((_,q,_,_),0,1) => q
+      case ((_,_,q,_),1,0) => q
+      case ((_,_,_,q),1,1) => q
       case _ => MatrixErrors.outOfBounds
     }
-  }
-  def updated(i: Int, j: Int, f: A => A): QuadTreeSquareMatrix[A] =
-    QT_Succ(subm.updated(i/2, j/2, (q:Tup) =>
-      (q, i%2, j%2) match {
-        case ((a,b,c,d),0,0) => (f(a),b,c,d)
-        case ((a,b,c,d),0,1) => (a,f(b),c,d)
-        case ((a,b,c,d),1,0) => (a,b,f(c),d)
-        case ((a,b,c,d),1,1) => (a,b,c,f(d))
+    def updated(i: Int, j: Int, f: A => A): M[A] =
+      Succ(next.updated(i/2, j/2, q => (q, i%2, j%2) match {
+        case ((a,b,c,d),0,0) => (f(a),  b ,  c ,  d )
+        case ((a,b,c,d),0,1) => (  a ,f(b),  c ,  d )
+        case ((a,b,c,d),1,0) => (  a ,  b ,f(c),  d )
+        case ((a,b,c,d),1,1) => (  a ,  b ,  c ,f(d))
         case _ => MatrixErrors.outOfBounds
       }))
-  def map[B](f: A => B): QuadTreeSquareMatrix[B] =
-    QT_Succ(subm.map {
-      q:Tup => (f(q._1), f(q._2), f(q._3), f(q._4))
-    })
-}
-
-object QuadTreeSquareMatrix extends SquareMatrixFactory[QuadTreeSquareMatrix] {
-  def fill[A](n: Int)(elem: => A): QuadTreeSquareMatrix[A] = {
-    if(n == 1) QT_Zero(elem)
-    else {
-      require(n > 0 && n % 2 == 0)
-      QT_Succ(fill(n/2, n/2)(elem,elem,elem,elem))
-    }
+    def map[B](f: A => B): M[B] =
+      Succ(next.map {
+        case (a,b,c,d) => (f(a), f(b), f(c), f(d))
+      })
   }
-  def tabulate[A](n:Int)(f:(Int,Int)=>A): QuadTreeSquareMatrix[A] = {
-    if(n == 1) QT_Zero(f(0,0))
+
+  def tabulate[A](n:Int)(f:(Int,Int)=>A): M[A] = {
+    if(n == 1) Zero(f(0,0))
     else {
       require(n > 0 && n % 2 == 0)
-      QT_Succ(tabulate(n/2, n/2) { (i,j) =>
+      Succ(tabulate(n/2, n/2) { (i,j) =>
         (f(2*i, 2*j),
          f(2*i, 2*j+1),
          f(2*i+1, 2*j),
@@ -115,30 +109,115 @@ object QuadTreeSquareMatrix extends SquareMatrixFactory[QuadTreeSquareMatrix] {
   }
 }
 
-object FastExpSquareMatrix {
-  abstract class Vec[A] {
-    def size: Int
-    def sub(i: Int): A
-  }
-  sealed case class Empty[A]() extends Vec[A] {
-    val size = 0
-    def sub(i:Int) = MatrixErrors.outOfBounds
-  }
-  sealed case class Id[A](value: A) extends Vec[A] {
-    val size = 1
-    def sub(i:Int) = if(i == 0) value else MatrixErrors.outOfBounds
-  }
-  sealed case class Pr[V[A]<:Vec[A], W[A]<:Vec[A], A](v: V[A], w: W[A])
-       extends Vec[A] {
-         val size = v.size + w.size
-         def sub(i:Int) =
-           if(i < v.size) v.sub(i)
-           else w.sub(i - v.size)
-       }
-  trait Pair[V[A]<:Vec[A], W[A]<:Vec[A]] {
-    type T[A] = Pr[V,W,A]              // for partial type application
+object SizedVectors {
+  /* Could implement these with case classes, but want to keep them
+     very lightweight: just nested pairs with no superfluous
+     wrappers. */
+  type Empty[A] = Unit
+  type Id[A] = A
+  type Two[V[_], W[_], A] = Pair[V[A], W[A]]
+
+  /** Partial application of `Two` type constructor. */
+  trait Tw[V[_], W[_]] {
+    type T[A] = Two[V,W,A]
   }
 
+  /** Constructors for sized vectors.  Written in this form (rather
+      than just functions) to maintain [rank-2] polymorphism when
+      passing them as arguments. */
+  trait Maker[V[_]] {
+    def apply[A](f: Int => A): V[A]
+  }
+
+  val mkE = new Maker[Empty] {
+    def apply[A](f: Int => A): Empty[A] = ()
+  }
+
+  val mkI = new Maker[Id] {
+    def apply[A](f: Int => A): Id[A] = f(0)
+  }
+
+  case class mkP[V[_], W[_]](mkv: Maker[V], mkw: Maker[W], vsize: Int)
+       extends Maker[Tw[V,W]#T] {
+         def apply[A](f: Int => A): Two[V,W,A] = (mkv(f), mkw(i => f(i + vsize)))
+       }
+
+  /* Sized vector subscript functions. */
+  def subE[A](i: Int, e: Empty[A]): A = MatrixErrors.outOfBounds
+
+  def subI[A](i: Int, x: Id[A]): A =
+    if(i == 0) x
+    else MatrixErrors.outOfBounds
+
+  def subP[V[_], W[_], A]
+    (subv: (Int,V[A])=>A, subw: (Int,W[A])=>A, vsize: Int)
+    (i: Int, p: Two[V,W,A]) =
+       if(i < vsize) subv(i, p._1)
+       else subw(i-vsize, p._2)
+
+  /* Some examples: singletons represented as
+   * fastexp E I 1 = fastexp Tw[E,I] Tw[I,I] 0 = Tw[E,I]
+   */
+  type T1[A] = Two[Empty,Id,A]
+  def mk1[A](f: Int => A): T1[A] = mkP(mkE,mkI,0)(f)
+  def sub1[A](i: Int, v: T1[A]): A = subP(subE, subI[A], 0)(i, v)
+  assert(sub1(0,mk1{x => 'Q'}) == 'Q')
+
+  /* Pairs represented as
+   * fastexp E I 2 = fastexp E Tw[I,I] 1 = fastexp Tw[E,Tw[I,I]] _ 0 = Tw[E,Tw[I,I]]
+   */
+  type T2[A] = Two[Empty,Tw[Id,Id]#T,A]
+  def mk2[A](f: Int => A): T2[A] = mkP[Empty,Tw[Id,Id]#T](mkE,mkP(mkI,mkI,1),0)(f)
+  def sub2[A](i: Int, v: T2[A]): A =
+    subP[Empty,Tw[Id,Id]#T,A](subE, subP[Id,Id,A](subI, subI, 1), 0)(i,v)
+//  assert(sub2(0,mk2{x => x
+}
+
+/*
+object FastExpSquareMatrix {
+
+  type Empty[A] = Unit
+  type Id[A] = A
+  type Two[V[_], W[_], A] = Pair[V[A], W[A]]
+
+  trait Tw[V[_], W[_]] {                // partial application of Two
+    type T[A] = Two[V,W,A]
+  }
+
+
+
+  type Square[A] = Square_[Empty,Id,A]
+
+  sealed abstract class Square_[V[_], W[_], A] extends SquareMatrix[A]
+
+  case class FE_Zero[V[_], W[_], A] private (data: V[V[A]])
+       extends Square_[V,W,A]
+  {
+    def dimension: Int = MatrixErrors.notImplemented
+    def apply(i: Int, j: Int): A = MatrixErrors.notImplemented
+    def map[B](f: A => B): Square[B] = MatrixErrors.notImplemented
+    def updated(i: Int, j: Int, elem: A): Square[A] = MatrixErrors.notImplemented
+  }
+
+  case class FE_Even[V[_], W[_], A] private (next: Square_[V, Tw[W,W]#T, A])
+       extends Square_[V,W,A]
+  {
+    def dimension: Int = MatrixErrors.notImplemented
+    def apply(i: Int, j: Int): A = MatrixErrors.notImplemented
+    def map[B](f: A => B): Square[B] = MatrixErrors.notImplemented
+    def updated(i: Int, j: Int, elem: A): Square[A] = MatrixErrors.notImplemented
+  }
+
+  case class FE_Odd[V[_], W[_], A] private (next: Square_[Tw[V,W]#T, Tw[W,W]#T, A])
+       extends Square_[V,W,A]
+  {
+    def dimension: Int = MatrixErrors.notImplemented
+    def apply(i: Int, j: Int): A = MatrixErrors.notImplemented
+    def map[B](f: A => B): Square[B] = MatrixErrors.notImplemented
+    def updated(i: Int, j: Int, elem: A): Square[A] = MatrixErrors.notImplemented
+  }
+
+/*
   abstract class Square_[V[A]<:Vec[A], W[A]<:Vec[A], A] {
     def sub(i: Int, j: Int): A
   }
@@ -192,5 +271,6 @@ object FastExpSquareMatrix {
 //  def mkP[V[A]<:Vec[A], W[A]<:Vec[A], A]
 //    (mkv: A=>V[A], mkw: A=>W[A], x: A): Pr[V,W,A] =
 //      Pr(mkv(x), mkw(x))
-
+*/
 }
+*/
--- src/test/scala/SquareMatrixTest.scala
+++ src/test/scala/SquareMatrixTest.scala
@@ -1,37 +1,40 @@
 import org.scalacheck._
 import org.scalacheck.Prop._
 
-object QuadTreeCreateSpec extends Properties("quad tree create") {
+trait SquareMatrixTestParams {
+  type M[A] = factory.M[A]
+  val factory: SquareMatrixFactory
+  val dimension: Gen[Int]
 
-  type M[A] = QuadTreeSquareMatrix[A]
-  val factory = QuadTreeSquareMatrix
+  def filled[A](elem: Gen[A]): Gen[M[A]] =
+    dimension.flatMap(n => elem.map(factory.fill(n)(_)))
 
-  val powerOfTwo = Gen.choose(0,6).map(1 << _)
-  val fillMatrix = for { n <- powerOfTwo ; c <- Gen.alphaChar }
-                   yield factory.fill(n)(c)
-  def pairs(n: Int) = factory.tabulate(n){(i,j)=>(i,j)}
-  val pairMatrix = powerOfTwo.map(pairs)
-  def coord[A](m: M[A]) = for {i <- Gen.choose(0, m.height-1);
-                               j <- Gen.choose(0, m.width-1)}
-                          yield (i,j)
+  def pairs(dim: Gen[Int]): Gen[M[(Int,Int)]] =
+    dim.flatMap(factory.tabulate(_)(Pair.apply _))
 
-  property("bounds") = Prop.forAll(fillMatrix) {
-    m:M[Char] => Prop.forAll(coord(m)) {
-      ij =>
-        ij._1 >= 0 && ij._2 >= 0 ==> // mysteriously, sometimes gets -1
-          (m(ij) == m(0,0))
+  def coord[A](m: Matrix[A]) =
+    Gen.choose(0, m.height-1).map2(Gen.choose(0, m.width-1))(Pair.apply _)
+}
+
+class SquareMatrixSpec(pm: SquareMatrixTestParams, nm: String)
+extends Properties(nm) {
+  import pm._
+  property("bounds") =
+    Prop.forAll(filled(Gen.alphaChar)) { m: M[Char] =>
+      Prop.forAll(coord(m)) { ij =>
+        m(ij) == m(0,0)
+      }
     }
-  }
 
-  property("coords") = Prop.forAll(pairMatrix) {
-    m => Prop.forAll(coord(m)) {
-      ij =>
-        ij._1 >= 0 && ij._2 >= 0 ==> // mysteriously, sometimes gets -1
-          (m(ij) == ij)
+  property("coords") =
+    Prop.forAll(pairs(dimension)) { m =>
+      Prop.forAll(coord(m)) { ij =>
+//        ij._1 >= 0 && ij._2 >= 0 ==> // mysteriously, sometimes gets -1
+        (m(ij) == ij)
+      }
     }
-  }
 
-  property("update") = Prop.forAll(fillMatrix) {
+  property("update") = Prop.forAll(filled(Gen.alphaChar)) {
     m => Prop.forAll(coord(m)) {
       ij =>
         val m2 = m.updated(ij._1, ij._2, '!')
@@ -41,7 +44,7 @@ object QuadTreeCreateSpec extends Properties("quad tree create") {
     }
   }
 
-  property("map") = Prop.forAll(pairMatrix) {
+  property("map") = Prop.forAll(pairs(dimension)) {
     m =>
       def f(xy: (Int,Int)) = xy._1 - xy._1
       val m2 = m.map(f)
@@ -49,4 +52,98 @@ object QuadTreeCreateSpec extends Properties("quad tree create") {
         ij => f(m(ij)) == m2(ij)
       }
   }
+
+}
+
+object QuadTreeTestParams extends SquareMatrixTestParams {
+  val factory = QuadTreeSquareMatrix
+  val dimension = Gen.choose(0,6).map(1 << _) // powers of two
+}
+
+object QuadTreeSpec extends SquareMatrixSpec(QuadTreeTestParams, "QuadTreeSpec")
+
+abstract class SizedVectorSpec(nm: String) extends Properties(nm) {
+  type T[_]
+  def size: Int
+  def mk[A](f: Int => A): T[A]
+  def sub[A](i: Int, v:T[A]): A
+
+  def frob(x: Int) = x * (x+1)
+  val sample = mk(frob)
+
+  property("elements") = Prop.forAll(Gen.choose(0,size-1)) {
+    i => sub(i,sample) == frob(i)
+  }
+}
+
+object SingletonVectorSpec extends SizedVectorSpec("singleton") {
+  import SizedVectors._
+  /* fastexp E I 1 = fastexp Tw[E,I] Tw[I,I] 0 = Tw[E,I] */
+  type T[A] = Two[Empty,Id,A]
+  def mk[A](f: Int => A): T[A] = mkP(mkE,mkI,0)(f)
+  def sub[A](i: Int, v: T[A]): A = subP(subE, subI[A], 0)(i, v)
+  def size = 1
+}
+
+object PairVectorSpec extends SizedVectorSpec("pair") {
+  import SizedVectors._
+  /* fastexp E I 2 =
+   * fastexp E Tw[I,I] 1 =
+   * fastexp Tw[E,Tw[I,I]] _ 0 =
+   * Tw[E,Tw[I,I]]
+   */
+  type T[A] = Two[Empty,Tw[Id,Id]#T,A]
+  def mk[A](f: Int => A): T[A] = mkP[Empty,Tw[Id,Id]#T](mkE,mkP(mkI,mkI,1),0)(f)
+  def sub[A](i: Int, v: T[A]): A =
+    subP[Empty,Tw[Id,Id]#T,A](subE, subP(subI[A], subI[A], 1), 0)(i,v)
+  def size = 2
+}
+
+object TripleVectorSpec extends SizedVectorSpec("triple") {
+  import SizedVectors._
+  /* fastexp E I 3 =
+   * fastexp Tw[E,I] Tw[I,I] 1 =
+   * fastexp Tw[Tw[E,I],Tw[I,I]] _ 0 =
+   * Tw[Tw[E,I],Tw[I,I]]
+   */
+  type T[A] = Two[Tw[Empty,Id]#T, Tw[Id,Id]#T, A]
+  def mk[A](f: Int => A): T[A] =
+    mkP[Tw[Empty,Id]#T, Tw[Id,Id]#T](
+      mkP(mkE,mkI,0),
+      mkP(mkI,mkI,1),
+      1)(f)
+  def sub[A](i: Int, v: T[A]): A =
+    subP[Tw[Empty,Id]#T, Tw[Id,Id]#T, A](
+      subP(subE,subI[A],0),
+      subP(subI[A],subI[A],1),
+      1)(i,v)
+  def size = 3
+}
+
+object QuadVectorSpec extends SizedVectorSpec("quad") {
+  import SizedVectors._
+  /* fastexp E I 4 =
+   * fastexp E Tw[I,I] 2 =
+   * fastexp E Tw[Tw[I,I],Tw[I,I]] 1 =
+   * fastexp Tw[E,Tw[Tw[I,I],Tw[I,I]]] _ 0 =
+   * Tw[E,Tw[Tw[I,I],Tw[I,I]]]
+   */
+  type T[A] = Two[Empty, Tw[Tw[Id,Id]#T, Tw[Id,Id]#T]#T, A]
+  def mk[A](f: Int => A): T[A] =
+    mkP[Empty, Tw[Tw[Id,Id]#T, Tw[Id,Id]#T]#T](
+      mkE,
+      mkP[Tw[Id,Id]#T, Tw[Id,Id]#T](
+        mkP(mkI,mkI,1),
+        mkP(mkI,mkI,1),
+        2),
+      0)(f)
+  def sub[A](i: Int, v: T[A]): A =
+    subP[Empty, Tw[Tw[Id,Id]#T, Tw[Id,Id]#T]#T, A](
+      subE,
+      subP[Tw[Id,Id]#T, Tw[Id,Id]#T, A](
+        subP(subI[A], subI[A], 1),
+        subP(subI[A], subI[A], 1),
+        2),
+      0)(i,v)
+  def size = 4
 }
